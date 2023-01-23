f8ffa05d20fd3725d9d7219871d4299b07e5b0fd
Ayoub-mtn <72629595+Ayoub-mtn@users.noreply.github.com>
2023-01-23 15:10:40 +0100

    test1

---
+++ Sudoku.GeneticAlgorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sudoku.Shared;
+
+namespace Sudoku.GeneticAlgorithm
+
+{
+    public class Program :  ISolverSudoku
+    {
+        public GridSudoku Solve(GridSudoku s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
